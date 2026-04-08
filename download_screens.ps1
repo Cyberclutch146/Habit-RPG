00dd246c958b43017229bbadd2feb07de354dc47
Cyberclutch146 <blazingswagata@gmail.com>
2026-04-08 15:14:26 +0530

    Initial Commit

---
+++ download_screens.ps1
@@ -0,0 +1,18 @@
+$screens = @(
+    @{ name = "Dashboard"; title = "Dashboard"; imgUrl = "https://lh3.googleusercontent.com/aida/ADBb0uiNEE0_M2jWdjJLftMVoJFtLXaUEbo0KreC_CLkXcxdc_GCoMSKZk3GCVdmDXdC89VWVWxb5DxgFjMg6z0qGKkuh6ErmJOMO38fn1FGVj9UxJVKfQ_WaWjgi5eTZqVxDTfLlBjHpfoRKWL7683vVHAdJ3LbZE4wrAl3dEYxStkGvs9biGb3Gdl0Go8DwxOCXV1wJej_H2gfUyeX5havJeMZGArdTcEcZmy5xJweAkaGCwQf3QQ2QAIiG7g"; htmlUrl = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sX2U5ZDU2YjYxZDg1ZjQwZjhhZTQwMmVhOWY2NmRkMTVmEgsSBxCSu4n1xwsYAZIBIwoKcHJvamVjdF9pZBIVQhMyODM3NDg5Mzg1OTc2MDAyOTk3&filename=&opi=89354086" },
+    @{ name = "Boss Battle"; title = "Boss Battle"; imgUrl = "https://lh3.googleusercontent.com/aida/ADBb0uicVgTL77Cn4cwKpvPwmWrHLzvs8uI7IiNQMj2i0xTMcVv7bD6AA4ZnLvwOxYoaj868mhVfLuECYH3mz-2nGsaIoEHGHV9ayEo6_AtwOLjP0EsbVu63S8bGIVt90TFQhMdT1-V42OQHu-0eg8_WUmuX180nH-slnplpkyHMXLJOK4lj6oO5SUnLjWaiFIj5Cefgs2KxjjQg4mXOOXl9gWsaYGSB5P183AL9TSXZTlkk8cWQ5_aINAiZtg0"; htmlUrl = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sX2YyOTI3MDg1Y2VlNzQzNjViOTNlODBlOGVhMTJiNTMyEgsSBxCSu4n1xwsYAZIBIwoKcHJvamVjdF9pZBIVQhMyODM3NDg5Mzg1OTc2MDAyOTk3&filename=&opi=89354086" },
+    @{ name = "Progress Stats"; title = "Progress Stats"; imgUrl = "https://lh3.googleusercontent.com/aida/ADBb0ugM0yoPUaYt1BAh9WIeNaNGZMR3r_IxYkU_M_SH6GDCx1vcw_FMR31iuIU_bBabgzg7kOoEsAIEl9aBpOkoIgNHLAQMpxYfuOhNs5SNY528fSqGBTC3aaHP0bsXja6b7xfiTDRI1oQLlrctUF-MAPFOU1Apl_Dyh8U5cREUvpNHVqMkcLJ_xfrMUTnzjemtVXgN_KMLXmg5dI4wlEi5qc8jlVPLTC1psD4crBGdVm81sbqx1XFUS1guYA"; htmlUrl = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzVkZDljNmIwMGFjZDQzOTlhNWFmOTgzNGNkMTU0OTg3EgsSBxCSu4n1xwsYAZIBIwoKcHJvamVjdF9pZBIVQhMyODM3NDg5Mzg1OTc2MDAyOTk3&filename=&opi=89354086" },
+    @{ name = "Onboarding"; title = "Onboarding"; imgUrl = "https://lh3.googleusercontent.com/aida/ADBb0uj9D1uAneoVi-Ihj-bpNWZrGMJE3McY18dO7pfbSVKkGJAjo4mKFghG7Yrv2zX2184V-hiNGbnJ0xSv_u-oWvEeASSUOYzZUHkH_6IbB4hmjNl3yFwK8Khek3pB0XM1mpelYHOxVGP4FMrSt1MJfHuyfXf7HCWCd4yMVPUp_WAhBSKgaFlUrLOQvLSh3ZV80vu0iGMbr-MZQlBRvkgc2DMIYev6jkfs3HSeacErPeOu186hJYrfESS_uoU"; htmlUrl = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sX2QyZDY5MDM5MTI3NzQ0ZDg4NDFlMmQ3ZTFmYTg1MWMwEgsSBxCSu4n1xwsYAZIBIwoKcHJvamVjdF9pZBIVQhMyODM3NDg5Mzg1OTc2MDAyOTk3&filename=&opi=89354086" },
+    @{ name = "Character Profile"; title = "Character Profile"; imgUrl = "https://lh3.googleusercontent.com/aida/ADBb0uhZRuXmzx_YVrYnKJHdAqM8E9vOrgm5ANqRKVJwqJdqQgly4AY4_tKU7RXqEQVRQaeDRBKBCKSNPh4wMppCnavKHMN6B1Z6ytX_MgOlegF5iP1HvsymKpLhIy5LFBRKMWjWm022B8w-DqfZWzw364IglgOCDnEQSqEpiTBRiDFg9HlV168oTkpJmsgvf1TyQdl2jCaN4Cz_H3tezVHORqhq4gueTyazY0lP9LX-miwET0enRSEfYblzFbk"; htmlUrl = "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ7Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpaCiVodG1sXzcxNWY3YmZiNDAxZjQ1MmI5ODk5ZTgyNGJiN2YwNjkwEgsSBxCSu4n1xwsYAZIBIwoKcHJvamVjdF9pZBIVQhMyODM3NDg5Mzg1OTc2MDAyOTk3&filename=&opi=89354086" }
+)
+
+foreach ($screen in $screens) {
+    $cleanTitle = $screen.title -replace ' ', '_'
+    $imgFile = "$cleanTitle.png"
+    $htmlFile = "$cleanTitle.html"
+    
+    Write-Host "Downloading $cleanTitle..."
+    curl.exe -s -L -o $imgFile $screen.imgUrl
+    curl.exe -s -L -o $htmlFile $screen.htmlUrl
+}
+Write-Host "Done!"
